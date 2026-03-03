185ea81f9bfe7fc11f7bbfb02e24a917ec62ee95
Pascal Keßler <pk375@students.uni-freiburg.de>
2026-03-03 08:42:08 +0100

    first throw

---
+++ problem_1.c
@@ -0,0 +1,125 @@
+//
+// Copyright (c) 2026 Pascal Keßler - All rights reserved.
+//
+
+// Needed to use qsort to check if the radix sort works correctly
+#include <stdlib.h>
+#include "mt19937-64.c"
+#include <omp.h>
+#include <string.h>
+
+#define NUMBER_OF_RUNS 10
+
+// TODO: REMOVE
+void printBitsRange(unsigned long num, int startBit, int endBit)
+{
+    for (int i = startBit; i >= endBit; i--)
+    {
+        printf("%lu", (num >> i) & 1);
+    }
+    printf("\n");
+}
+
+/**
+ * Comparator for the qsort function, just for check if the radix sort works correctly
+ * @param a
+ * @param b
+ * @return
+ */
+int compare(const void* a, const void* b)
+{
+    const unsigned long ua = *(unsigned long*)a;
+    const unsigned long ub = *(unsigned long*)b;
+    if (ua < ub) return -1;
+    if (ua > ub) return 1;
+    return 0;
+}
+
+int isPowerOfTwo(const int b)
+{
+    return b > 0 && (b & b - 1) == 0;
+}
+
+/**
+ *
+ * @param numbersToSort as unsigned long values
+ * @param n number of elements to sort
+ * @param b number of bits per element
+ */
+double sortArray_withRadixSort(unsigned long numbersToSort[], unsigned long numbersToSwap[], const long n, const int b)
+{
+    const int numberOfBuckets = 1 << b;
+    int bucketSizes[numberOfBuckets];
+    int bucketStart[numberOfBuckets];
+
+    const double start_time = omp_get_wtime();
+    for (int bitrange = 0; bitrange < 64; bitrange += b)
+    {
+        memset(bucketSizes, 0, sizeof(bucketSizes));
+        memset(bucketStart, 0, sizeof(bucketStart));
+
+        const unsigned long bitMaskForBitrange = (1UL << b) - 1;
+        for (long i = 0; i < n; i += 1)
+        {
+            bucketSizes[numbersToSort[i] >> bitrange & bitMaskForBitrange]++;
+        }
+        for (long i = 1; i < numberOfBuckets; i++)
+        {
+            bucketStart[i] = bucketStart[i - 1] + bucketSizes[i - 1];
+        }
+        for (long i = 0; i < n; i += 1)
+        {
+            numbersToSwap[
+                bucketStart[numbersToSort[i] >> bitrange & bitMaskForBitrange]++
+            ] = numbersToSort[i];
+        }
+
+        unsigned long* temp = numbersToSort;
+        numbersToSort = numbersToSwap;
+        numbersToSwap = temp;
+    }
+    return omp_get_wtime() - start_time;
+}
+
+
+int main(int argc, char* argv[])
+{
+    const long n = argc > 1 ? atoi(argv[1]) : 100000;
+    const int b = argc > 2 ? atoi(argv[2]) : 8;
+    if (b == 1 || !isPowerOfTwo(b) || b > 32)
+    {
+        printf("Please insert b, so b is power of 2\n");
+        return 1;
+    }
+
+    unsigned long numbersToSort[n];
+    unsigned long numbersToSwap[n];
+    unsigned long numbersSorted[n];
+
+    double totalTime = 0;
+    for (int i = 0; i < NUMBER_OF_RUNS; i++)
+    {
+        // To make n > 300000 possible
+        // unsigned long* numbersToSort = aligned_alloc(64, n * sizeof(unsigned long));
+        // unsigned long* numbersToSwap = aligned_alloc(64, n * sizeof(unsigned long));
+        // unsigned long* numbersSorted = aligned_alloc(64, n * sizeof(unsigned long));
+
+        for (long g = 0; g < n; g++)
+        {
+            numbersToSort[g] = genrand64_int64();
+            numbersSorted[g] = numbersToSort[g];
+        }
+
+        totalTime += sortArray_withRadixSort(numbersToSort, numbersToSwap, n, b);
+
+        qsort(numbersSorted, n, sizeof(unsigned long), compare);
+        for (long j = 0; j < n; j++)
+        {
+            if (numbersSorted[j] == numbersToSort[j]) continue;
+            printf("Error at index %lu: expected %lu, got %lu\n", j, numbersSorted[j], numbersToSort[j]);
+            return 1;
+        }
+    }
+    printf("Average time taken: %f seconds\n", totalTime / NUMBER_OF_RUNS);
+    return 0;
+}
---
+++ problem_4.c
@@ -0,0 +1,8 @@
+//
+// Copyright (c) 2026 Pascal Keßler - All rights reserved.
+//
+
+
+int main(void)
+{
+}
