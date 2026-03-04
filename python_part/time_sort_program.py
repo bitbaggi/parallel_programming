6f121791f8effacbb2fb964f7cf0d92cf8ff2bab
Pascal Keßler <pk375@students.uni-freiburg.de>
2026-03-04 20:00:52 +0100

    added script for task 4

---
+++ python_part/time_sort_program.py
@@ -0,0 +1,36 @@
+import subprocess
+from timing_helper import parse_timing_output
+
+def run_sort_program(program: str,
+                     bitsToTest: [int],
+                     nToTest: [int],
+                     maxSeconds: float,
+                     customEnv: dict) -> dict:
+    timings = {}
+    for bits in bitsToTest:
+        for count_n in nToTest:
+            try:
+                result = subprocess.run(
+                    [program, str(count_n), str(bits)],
+                    capture_output=True,
+                    timeout=300,
+                    env=customEnv
+                )
+                if result.returncode != 0:
+                    print(f"Fehler (exit {result.returncode}):")
+                    print(result.stderr.decode())
+                    print(result.stdout.decode())
+                    continue
+                result = parse_timing_output(result.stdout.decode())
+                print(f"{bits},{count_n} done in {result[len(result) - 1][1]:.2f}s")
+                if result[len(result) - 1][1] > maxSeconds:
+                    print(f"greater 10s -> next bits")
+                    break
+                timings[(bits, count_n)] = result
+            except subprocess.TimeoutExpired as e:
+                print(f"Timeout -> next bits")
+                break
+    return timings
+
+if __name__ == "__main__":
+    print("This is the time_sort_program.py file and is not meant to run by itself.")
