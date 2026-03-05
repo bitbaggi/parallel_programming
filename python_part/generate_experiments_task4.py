89f41465940b5309c88c8f8a9867d3d12ca9dbc3
Pascal Keßler <pk375@students.uni-freiburg.de>
2026-03-05 09:17:58 +0100

    added script for task 4

--- python_part/generate_experiments_task4.py
+++ python_part/generate_experiments_task4.py
@@ -7,8 +7,9 @@ from time_sort_program import run_sort_program
 # STRONG SCALING:
 THREAD_COUNTS = [1, 2, 4, 8, 16, 32]
 BITS_TO_TEST = [8]
-N_TO_TEST = lambda p: {n for n in range(MAXIMUM_N_SEQ, MAXIMUM_N_SEQ * p, MAXIMUM_N_SEQ)}
+N_TO_TEST = lambda p: {n for n in range(MAXIMUM_N_SEQ, MAXIMUM_N_SEQ * p + 1 , MAXIMUM_N_SEQ)}
 
+print(N_TO_TEST(1))
 for threadCount in THREAD_COUNTS:
     print("Run sort program")
     timings = run_sort_program('../problem_4', BITS_TO_TEST, N_TO_TEST(threadCount), MAX_SECONDS,
