200e9e2a6bda67a52fbada606116f6388e9d85b6
Pascal Keßler <pk375@students.uni-freiburg.de>
2026-03-05 09:16:17 +0100

    change for weak scaling

--- python_part/generate_experiments_task4.py
+++ python_part/generate_experiments_task4.py
@@ -1,4 +1,4 @@
-MAXIMUM_N_SEQ = 100000
+MAXIMUM_N_SEQ = 470000000
 MAX_SECONDS = 10
 
 from timing_helper import write_timings_to_file
@@ -6,12 +6,12 @@ from time_sort_program import run_sort_program
 
 # STRONG SCALING:
 THREAD_COUNTS = [1, 2, 4, 8, 16, 32]
-BITS_TO_TEST = [1, 2, 4, 8, 16]
-numberCount = MAXIMUM_N_SEQ
+BITS_TO_TEST = [8]
+N_TO_TEST = lambda p: {n for n in range(MAXIMUM_N_SEQ, MAXIMUM_N_SEQ * p, MAXIMUM_N_SEQ)}
 
 for threadCount in THREAD_COUNTS:
     print("Run sort program")
-    timings = run_sort_program('../problem_4', BITS_TO_TEST, [MAXIMUM_N_SEQ], MAX_SECONDS,
+    timings = run_sort_program('../problem_4', BITS_TO_TEST, N_TO_TEST(threadCount), MAX_SECONDS,
                                customEnv={'OMP_NUM_THREADS': str(threadCount)})
     write_timings_to_file(f'./results/timings_parallel_{threadCount}_threads.csv', timings)
     print(f"ThreadCount {threadCount} done")
