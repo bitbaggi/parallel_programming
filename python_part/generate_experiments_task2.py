05bb2486855dae954af407cfbdcea29385f0739b
Pascal Keßler <pk375@students.uni-freiburg.de>
2026-03-07 19:28:13 +0100

    added task2

---
+++ python_part/generate_experiments_task2.py
@@ -0,0 +1,13 @@
+from time_sort_program import run_sort_program
+from timing_helper import write_timings_to_file
+
+MAX_SECONDS = 10
+STEP_SIZE = 50000000
+MAX_NUMBER_COUNT = 10000000000
+BIT_COUNTS_TO_TEST = [1, 2, 4, 8, 16]
+
+seq_timings = run_sort_program('../problem_1', BIT_COUNTS_TO_TEST,
+                               [n for n in range(100000000, MAX_NUMBER_COUNT, STEP_SIZE)],
+                               MAX_SECONDS, customEnv={})
+
+write_timings_to_file(f'./results/sequential/strong_scaling/1_threads.csv', seq_timings)
